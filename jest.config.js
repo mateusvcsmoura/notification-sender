5c898d1522013e9bcb2179071e84513ad1abbace
Mateus Moura <mateusvcsmoura@gmail.com>
2025-10-11 19:50:30 -0300

    test: add tests for e-mail service

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
 };
